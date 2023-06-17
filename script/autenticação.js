e36b589938bcc7641df5fff1e622bef55465f699
KaykeBauerPC <bauer.kaykemarins@gmail.com>
2023-06-17 11:44:58 -0300

    Add Firebase Authetication

---
+++ script/autenticação.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDMSTH5e32kwv82bCXFkxzztnGyYgqTGRI",
+  authDomain: "fatecast-b5ddc.firebaseapp.com",
+  projectId: "fatecast-b5ddc",
+  storageBucket: "fatecast-b5ddc.appspot.com",
+  messagingSenderId: "697483630100",
+  appId: "1:697483630100:web:3955c9cf4f79097ddb3901"
+};
