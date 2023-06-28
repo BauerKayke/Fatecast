9dfedb1b916bd2cf0fb692bda5f5d37ed9ff303a
KaykeBauerPC <bauer.kaykemarins@gmail.com>
2023-06-27 22:55:45 -0300

    Create User, Fix responsive

--- firebase_init.js
+++ firebase_init.js
@@ -1,27 +1,55 @@
 const firebaseConfig = {
-    apiKey: 'AIzaSyDMSTH5e32kwv82bCXFkxzztnGyYgqTGRI',
-    authDomain: 'fatecast-b5ddc.firebaseapp.com',
-    projectId: 'fatecast-b5ddc',
-    storageBucket: 'fatecast-b5ddc.appspot.com',
-    messagingSenderId: '697483630100',
-    appId: '1:697483630100:web:3955c9cf4f79097ddb3901'
-  }
-  firebase.initializeApp(firebaseConfig)
-
-function login(form){
-    console.log('Antes')
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(
-        form.email().value,
-        form.senha().value
-      )
-      .then(response => {
-        window.location.href="pages/home.html"
-        console.log('success', response)
-      })
-      .catch(error => {
-        console.log('erro', error)
-      })
-    console.log('Depois')
+  apiKey: 'AIzaSyDMSTH5e32kwv82bCXFkxzztnGyYgqTGRI',
+  authDomain: 'fatecast-b5ddc.firebaseapp.com',
+  projectId: 'fatecast-b5ddc',
+  storageBucket: 'fatecast-b5ddc.appspot.com',
+  messagingSenderId: '697483630100',
+  appId: '1:697483630100:web:3955c9cf4f79097ddb3901'
 }
+firebase.initializeApp(firebaseConfig);
+
+
+const form1 = document.querySelector('form')
+
+
+function login() {
+  validarEmail()
+  validarSenha()
+
+  console.log('Antes')
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(
+      form1.email.value,
+      form1.senha.value
+    )
+    .then(response => {
+      window.location.replace("/pages/home.html")
+      console.log('success', response)
+    })
+    .catch(error => {
+      console.log('erro', error)
+    })
+  console.log('Depois')
+}
+
+function validarEmail() {
+  if (form1.email.value === "") {
+    errorInput(form1.email, "Preencha o campo obrigatório!");
+  } else {
+    const formItem = email.parentElement;
+    formItem.className = "group-input";
+  }
+}
+function validarSenha() {
+  if (form1.senha.value === "" || form1.senha.value.length < 6) {
+    errorInput(form1.senha, "Senha inválida!");
+  } else {
+    const formItem = senha.parentElement;
+    formItem.className = "group-input";
+  }
+}
+
+
+
+
--- script/validarCadastro.js
+++ script/validarCadastro.js
@@ -1,80 +1,85 @@
 
-const form = document.getElementById("form");
+
+const form2 = document.getElementById("formCadastro");
 const nome = document.getElementById("nome");
 const email = document.getElementById("email");
 const ra = document.getElementById("ra");
 const senha = document.getElementById("senha");
 const senhaConf = document.getElementById("senhaConf");
+const emailValue = email.value;
+const senhaValue = senha.value;
 
-form.addEventListener("submit", (event) => {
+form2.addEventListener("submit", (event) => {
     event.preventDefault();
-
+    console.log(email.value)
     validarNome();
     validarEmail();
     validarRa();
     validarSenha();
     validarSenhaConf();
+    register()
 })
 
-function validarNome(){
+
+function validarNome() {
     const nomeValue = nome.value;
-    
-    if(nomeValue === ""){
+
+    if (nomeValue === "") {
         errorInput(nome, "Preencha o campo obrigatório!");
-    }else{
+    } else {
         const formItem = nome.parentElement;
         formItem.className = "group-input";
     }
 }
 
-function validarEmail(){
+function validarEmail() {
     const emailValue = email.value;
-    
-    if(emailValue === ""){
+
+    if (emailValue === "") {
         errorInput(email, "Preencha o campo obrigatório!");
-    }else{
+    } else {
         const formItem = email.parentElement;
         formItem.className = "group-input";
     }
 }
 
-function validarRa(){
+function validarRa() {
     const raValue = ra.value;
-    
-    if(raValue === ""){
+
+    if (raValue === "") {
         errorInput(ra, "Preencha o campo obrigatório!");
-    }else if(raValue.length !== 13){
+    } else if (raValue.length !== 13) {
         errorInput(ra, "Preencha o ra corretamente!");
-    }else{
+    } else {
         const formItem = ra.parentElement;
         formItem.className = "group-input";
     }
 }
 
-function validarSenha(){
+function validarSenha() {
     const senhaValue = senha.value;
-    
-    if(senhaValue === "" || senhaValue.length < 8){
+
+    if (senhaValue === "" || senhaValue.length < 6) {
         errorInput(senha, "Senha inválida!");
-    }else{
+    } else {
         const formItem = senha.parentElement;
         formItem.className = "group-input";
     }
 }
 
-function validarSenhaConf(){
+function validarSenhaConf() {
     const senhaValue = senha.value;
     const senhaConfValue = senhaConf.value;
-    
-    if(senhaConfValue === "" || senhaConfValue !== senhaValue){
+
+    if (senhaConfValue === "" || senhaConfValue !== senhaValue) {
         errorInput(senhaConf, "Senha inválida!");
-    }else{
+    } else {
         const formItem = senhaConf.parentElement;
         formItem.className = "group-input";
     }
 }
 
-function errorInput(input, message){
+function errorInput(input, message) {
     const formItem = input.parentElement;
     const textMessage = formItem.querySelector("a");
 
@@ -82,3 +87,21 @@ function errorInput(input, message){
     formItem.className = "group-input-error"
 }
 
+function register() {
+    firebase.auth().createUserWithEmailAndPassword(email.value, senha.value)
+        .then((userCredential) => {
+            // Novo usuário criado com sucesso
+            const user = userCredential.user;
+            console.log('Successfully created new user:', user.uid);
+        })
+        .catch((error) => {
+            alert(getErrorMessage(error));
+            console.log('Error creating new user:', error);
+        });
+}
+function getErrorMessage(error) {
+    if (error.code == "auth/email-already-in-use") {
+        return "Email já está em uso";
+    }
+    return error.message;
+}
